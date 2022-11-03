df00a013df3b880ee8a203ed6369a7295749b8fb
Emilly Sandy <emillysandy05@gmail.com>
2022-11-03 11:22:37 -0300

    Aula 1

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="colums is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-one-quarter">
+      <FormularioAlura />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from './components/BarraLateral.vue'
+import FormularioAlura from './components/FormularioAlura.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    FormularioAlura
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
